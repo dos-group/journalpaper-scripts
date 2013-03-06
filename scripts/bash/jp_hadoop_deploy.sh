f6867fdd9de0feea701766e61634234a20ad6e7b
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-03-06 05:39:42 +0100

    Updated deploy scripts.

--- scripts/bash/jp_hadoop_deploy.sh
+++ scripts/bash/jp_hadoop_deploy.sh
@@ -4,9 +4,12 @@
 #
 # Author: Alexander Alexandrov (alexander.alexandrov@tu-berlin.de)
 
+# load configuration
+. ./jp_configure.sh
+
 echo "Removing Hadoop install folder '${HADOOPMR_HOME}'." 
 rm -Rf ${HADOOPMR_HOME}
 
 echo "Deploying Hadoop version from '${HADOOPMR_TAR}' to '${HADOOPMR_HOME}'."
-tar -xzvf ${HADOOPMR_TAR} -C `dirname ${HADOOPMR_HOME}`
-chown -R ${EXP_USER}:${EXP_GROUP} ${STRATOSPHERE_HOME}
+tar -xzvf ${HADOOPMR_TAR} -C `dirname ${HADOOPMR_HOME}` > /dev/null
+chown -R ${EXP_USER}:${EXP_GROUP} ${HADOOPMR_HOME}
--- scripts/bash/jp_stratosphere_deploy.sh
+++ scripts/bash/jp_stratosphere_deploy.sh
@@ -4,9 +4,12 @@
 #
 # Author: Alexander Alexandrov (alexander.alexandrov@tu-berlin.de)
 
+# load configuration
+. ./jp_configure.sh
+
 echo "Removing Stratosphere install folder '${STRATOSPHERE_HOME}'." 
 rm -Rf ${STRATOSPHERE_HOME}
 
 echo "Deploying Stratosphere version from '${STRATOSPHERE_TAR}' to '${STRATOSPHERE_HOME}'."
-tar -xzvf ${STRATOSPHERE_TAR} -C  `dirname ${STRATOSPHERE_HOME}`
+tar -xzvf ${STRATOSPHERE_TAR} -C `dirname ${STRATOSPHERE_HOME}` > /dev/null
 chown -R ${EXP_USER}:${EXP_GROUP} ${STRATOSPHERE_HOME}
