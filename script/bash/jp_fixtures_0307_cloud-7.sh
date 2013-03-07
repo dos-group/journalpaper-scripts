262851f43e5462dc94791257770b805b8f784fac
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-03-07 19:43:13 +0100

    Prepared tera-sort and word-count fixtures for the 26-node cluster.

--- script/bash/jp_fixtures_0307_cloud-7.sh
+++ script/bash/jp_fixtures_0307_cloud-7.sh
@@ -7,7 +7,5 @@
 ./jp_sut_deploy.sh ${HDP_MAPR_TAR} ${HDP_MAPR_HOME}
 ./jp_sut_deploy.sh ${STR_PACT_TAR} ${STR_PACT_HOME}
 
-# start: March 6th, 14:15 AM;
-# end:   ???
 ./jp_run_wordcount.sh 4
 ./jp_run_terasort.sh 4
