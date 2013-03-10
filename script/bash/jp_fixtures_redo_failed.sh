40cc1da465a2d572b2cee46808bc91874bece1bd
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-03-10 17:34:39 +0100

    Re-deploying all systems at the start of the new redo failed job executions script.

--- script/bash/jp_fixtures_redo_failed.sh
+++ script/bash/jp_fixtures_redo_failed.sh
@@ -4,8 +4,8 @@
 . ./jp_env_configure.sh
 
 # deploy systems under test (SUTs)
-#./jp_sut_deploy.sh ${HDP_MAPR_TAR} ${HDP_MAPR_HOME}
-#./jp_sut_deploy.sh ${STR_PACT_TAR} ${STR_PACT_HOME}
+./jp_sut_deploy.sh ${HDP_MAPR_TAR} ${HDP_MAPR_HOME}
+./jp_sut_deploy.sh ${STR_PACT_TAR} ${STR_PACT_HOME}
 
 # set initial dop
 dopOld=2
