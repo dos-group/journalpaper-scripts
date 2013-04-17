5bbe90dbc811ebe5d931bfbc3a6500c4e88f8385
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-04-17 07:54:29 +0200

    Added TPCH Q3 result logs and plots.

--- util/evaluation/collect.sh
+++ util/evaluation/collect.sh
@@ -5,7 +5,7 @@
 DIR_RESULTS=../../result/final
 SCALE_FACTOR="0010 0020 0030 0040 0050"
 DOP="0040 0080 0120 0160 0200"
-EXPERIMENT="wc-str_pact wc_jst-str_pact wc-hdp_mapr ts-hdp_mapr ts-str_pact"
+EXPERIMENT="wc-str_pact wc_jst-str_pact wc-hdp_mapr ts-hdp_mapr ts-str_pact q3-hdp_hive q3-str_pact"
 
 for exp in $EXPERIMENT; do
 	OUTPUT_FILE="csv/$exp.csv"
