98848d8daeb95e6bc906f54d2b227800798df0fa
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-04-28 01:04:05 +0200

    Fixed wrong comments in jp_fixtures_0426_cloud-11.sh.

--- script/bash/jp_fixtures_0426_cloud-11.sh
+++ script/bash/jp_fixtures_0426_cloud-11.sh
@@ -7,13 +7,13 @@
 ./jp_sut_deploy.sh ${HDP_MAPR_TAR} ${HDP_MAPR_HOME}
 ./jp_sut_deploy.sh ${STR_PACT_TAR} ${STR_PACT_HOME}
 
-#  5 nodes
+#  25 nodes
 ./jp_run_trienum.sh 25
-# 10 nodes
+# 20 nodes
 ./jp_run_trienum.sh 20
 # 15 nodes
 ./jp_run_trienum.sh 15
-# 20 nodes
+# 10 nodes
 ./jp_run_trienum.sh 10
-# 25 nodes
+#  5 nodes
 ./jp_run_trienum.sh  5
