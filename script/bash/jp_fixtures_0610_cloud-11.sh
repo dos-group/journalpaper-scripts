8e71c3cb369ab658f48eaeaf13216dfca779f763
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2013-06-10 00:49:55 +0200

    Fixed a bug in the system parsing logic in jp_run_repeated.sh.

--- script/bash/jp_fixtures_0610_cloud-11.sh
+++ script/bash/jp_fixtures_0610_cloud-11.sh
@@ -4,17 +4,17 @@
 . ./jp_env_configure.sh
 
 # deploy systems under test (SUTs)
-#./jp_sut_deploy.sh ${HDP_MAPR_TAR} ${HDP_MAPR_HOME}
+./jp_sut_deploy.sh ${HDP_MAPR_TAR} ${HDP_MAPR_HOME}
 #./jp_sut_deploy.sh ${STR_PACT_TAR} ${STR_PACT_HOME}
 ./jp_sut_deploy.sh ${OZN_PACT_TAR} ${OZN_PACT_HOME}
 
 #  5 nodes
-#./jp_run_ts_otf.sh  5
+./jp_run_ts_otf.sh  5
 # 10 nodes
-#./jp_run_ts_otf.sh  10
+./jp_run_ts_otf.sh  10
 # 15 nodes
-#./jp_run_ts_otf.sh  15
+./jp_run_ts_otf.sh  15
 # 20 nodes
-#./jp_run_ts_otf.sh  20
+./jp_run_ts_otf.sh  20
 # 25 nodes
-#./jp_run_ts_otf.sh  25
+./jp_run_ts_otf.sh  25
